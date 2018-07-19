8b2aef90986b429e25dcfa6da37330b3fc4b0745
iNo0r <pic.ner101@gmail.com>
2018-07-19 09:53:10 +0800

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# ReactLifeCycleMethods
-Created with CodeSandbox
---
+++ src/index.js
@@ -0,0 +1,24 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
+
+// class App extends React.Component{
+
+//   componentWillMount(){
+//       alert('Comopnent WIll mount')
+//   }
+
+//   componentDidMount(){
+//       alert('comopnentdidMount')
+//   }
+
+// render(){
+//   return(
+//     <div>
+//       {alert('render')}
+//     </div>
+//   )
+// }
+// }
+
+// ReactDOM.render(<App/> ,document.getElementById('root'))
